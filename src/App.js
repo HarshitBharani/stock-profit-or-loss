af7917aea4f4a4fa75974bc973cd623e13e8f79a
Harshit Bharani <50341476+HarshitBharani@users.noreply.github.com>
2021-01-19 23:51:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  var [buyPrice, setBuyPrice] = useState("");
+
   return (
     <div className="App">
       <input placeholder="pls enter the stock price when ou bought it"></input>
