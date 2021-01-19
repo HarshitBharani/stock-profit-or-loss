dca0f151a09685e6e15c019235fcc1619ee57bd1
Harshit Bharani <50341476+HarshitBharani@users.noreply.github.com>
2021-01-19 23:57:53 +0530

    added price change handler

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import "./styles.css";
 
 export default function App() {
   var [buyPrice, setBuyPrice] = useState("");
+  function priceChangeHandler(event) {
+    var bPrice = event.target.value;
+    setBuyPrice(bPrice);
+  }
 
   return (
     <div className="App">
-      <input placeholder="pls enter the stock price when ou bought it"></input>
+      <input
+        onChange={() => priceChangeHandler(event)}
+        placeholder="pls enter the stock price when ou bought it"
+      ></input>
     </div>
   );
 }
