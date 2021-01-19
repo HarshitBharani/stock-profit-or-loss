d141774bac27842fe53a8c4d981146d470379f1b
Harshit Bharani <50341476+HarshitBharani@users.noreply.github.com>
2021-01-19 23:43:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <input placeholder="pls enter the stock price when ou bought it"></input>
+    </div>
+  );
+}
